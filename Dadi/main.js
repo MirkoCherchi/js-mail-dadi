e2f941aac112aaff7080e05d113d561ddda8829c
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-09 17:53:43 +0100

    feat: dadi a schermo

--- Dadi/main.js
+++ Dadi/main.js
@@ -1,16 +1,53 @@
 'use strict';
 
-const giocatore = Math.floor(Math.random() * 6) + 1;
-console.log('Numero Giocatore', giocatore);
 
-const computer = Math.floor(Math.random() * 6) + 1;
-console.log('Numero Computer', computer);
+const bottone = document.querySelector ('.btn');
+let risultato = document.querySelector ('.risultato')
 
-if (giocatore < computer) {
-    console.log('Computer win');
+
+bottone.addEventListener('click', function(){
     
-} else if (computer < giocatore) {
-    console.log('Giocatore win');
-} else {
-    console.log('Pareggio');
-}
+ const giocatore = Math.floor(Math.random() * 6) + 1;
+ console.log('Numero Giocatore', giocatore);
+ const computer = Math.floor(Math.random() * 6) + 1;
+ console.log('Numero Computer', computer);
+ const punteggioG = document.querySelector('.punteggiog');
+ punteggioG.innerHTML = giocatore;
+const punteggioC = document.querySelector('.punteggioc');
+punteggioC.innerHTML = computer;
+
+     
+if (giocatore < computer) {
+     console.log('Computer win');
+     risultato.innerHTML = 'Computer Win';
+    
+ } else if (computer < giocatore) {
+     console.log('Giocatore win');
+     risultato.innerHTML = 'Giocatore Win';
+ } else {
+     console.log('Pareggio');
+     risultato.innerHTML = 'Pareggio';
+ }
+})
+
+
+
+
+
+
+ 
+// const giocatore1 = Math.floor(Math.random() * 6) + 1;
+// const giocatore2 = Math.floor(Math.random() * 6) + 1;
+// console.log('Numero Giocatore', giocatore1 + giocatore2);
+// const computer1 = Math.floor(Math.random() * 6) + 1;
+// const computer2 = Math.floor(Math.random() * 6) + 1;
+// console.log('Numero Computer', computer1 + computer2);
+
+// if (giocatore1 + giocatore2 < computer1 + computer2) {
+//      console.log('Computer win');
+    
+// } else if (computer1 + computer2 < giocatore1 + giocatore2) {
+//      console.log('Giocatore win');
+//  } else {
+//      console.log('Pareggio');
+//  }
