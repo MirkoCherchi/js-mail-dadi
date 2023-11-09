88bd122a9ff9fc57e30332e6ec79ff40bc154143
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-09 16:02:12 +0100

    feat: space fix

--- Mail/main.js
+++ Mail/main.js
@@ -4,6 +4,7 @@ const emailRichiesta = prompt('Inserisci la tua Email')
 const emailArray = ['email1@gmail.com', 'email2@gmail.com', 'email3@gmail.com', 'email4@gmail.com', 'email5@gmail.com', 'email6@gmail.com', 'email7@gmail.com',]
 let emailCorretta = false;
 
+
 for (let verifica = 0; verifica < emailArray.length; verifica++){
     
     if (emailArray[verifica] === emailRichiesta) {
@@ -15,9 +16,10 @@ if (emailCorretta) {
     console.log('Email corretta: ', emailRichiesta);
     alert ('email corretta')
     
-} else
-    console.log('Email errata: ', emailRichiesta);
+} else{
+console.log('Email errata: ', emailRichiesta);
     alert ('email errata');
+}
 
     
 
